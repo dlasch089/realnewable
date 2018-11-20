c4c9c2315725345e912d10fc217fdd6187c25151
Dominik Lasch <dl@exutec.de>
2018-11-20 09:59:48 +0100

    added: pages and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,17 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ResultsComponent } from './components/results/results.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ResultPageComponent } from './pages/result-page/result-page.component';
+import { UserInputComponent } from './components/user-input/user-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ResultsComponent
+    ResultsComponent,
+    HomeComponent,
+    ResultPageComponent,
+    UserInputComponent
   ],
   imports: [
     BrowserModule,
