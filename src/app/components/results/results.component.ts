174a09f2df9338b5fa626a900040858872a9cd22
Dominik Lasch <dl@exutec.de>
2018-11-23 10:37:36 +0100

    added: other renewables per area

--- src/app/components/results/results.component.ts
+++ src/app/components/results/results.component.ts
@@ -29,9 +29,10 @@ export class ResultsComponent implements OnInit {
 
   shareArray:Array<Number> = [];
   sortedShareArray:Array<Number>;
-  sharePositions:Array<Number> = [];
+  rankedShares:Array<Number> = [];
 
   optimalTime:Number;
+  optimumToday:Number;
   
   areaCodes = {
     germany: ['Germany'],
@@ -48,14 +49,32 @@ export class ResultsComponent implements OnInit {
     won: 'wind-onshore'
   }
 
-  states:Array<String> = ['Baden-Wuerttemberg', 'Bavaria','Berlin','Brandenburg','Bremen','Hamburg','Hesse','Mecklenburg-Vorpommern','Lower Saxony','North Rhine-Westphalia','Rheinland-Pfalz','Saarland','Saxony','Saxony-Anhalt','Schleswig-Holstein','Thuringia'];
-
-  // Hourly share of Biomass in percentage; Position 0 = 0 am!
-  pumpedHydroShare:Array<number> = [0.0034, 0.0021, 0.0017, 0.0015, 0.0014, 0.0019, 0.0109, 0.0252, 0.0309, 0.0234, 0.0153, 0.0117, 0.0079, 0.0062, 0.0063, 0.0078, 0.0117, 0.0234, 0.0306, 0.0351, 0.0284, 0.0183, 0.0140, 0.0065]
- 
+  // Hourly share of pumped hydro storage in percentage; Position 0 = 0 am!
   // Average of hourly generation of water, biomass and other renewable sources
-  otherRenewables:number = 1778 + 4568 + 141;
-
+  otherRenewables:Object = {
+    germany: {
+      pumpedHydro: [181, 112, 87, 76, 75, 105, 628, 1523, 1964, 1549, 1047, 822, 557, 437, 431, 522, 769, 1514, 1948, 2187, 1725, 1078, 812, 363],
+      // Water, biomass, others: 1778 + 4568 + 141
+      others:  6487
+    },
+    tennet: {
+      pumpedHydro: [41, 22, 10, 8, 9, 16, 140, 316, 402, 352, 239, 185, 122, 92, 85, 114, 180, 359, 471, 503, 420, 289, 220, 103],
+      others: 3072
+    },
+    transnet: {
+      pumpedHydro: [27, 9, 4, 2, 2, 17, 329, 631, 636, 471, 319, 300, 189, 142, 177, 181, 296, 560, 707, 693, 449, 309, 190, 108],
+      others: 1004
+    },
+    amprion: {
+      pumpedHydro: [14, 7, 3, 2, 3, 12, 112, 215, 233, 163, 117, 108, 70, 55, 60, 75, 120, 216, 272, 266, 183, 119, 84, 57],
+      others: 1142
+    },
+    hertz: {
+      pumpedHydro: [91, 79, 71, 68, 71, 91, 256, 561, 632, 478, 303, 214, 157, 133, 139, 174, 280, 513, 662, 719, 522, 338, 220, 169],
+      others: 1268
+    }
+  }
+ 
   constructor(private energyDataService: EnergydataService) { }
   
   ngOnInit() {
@@ -65,22 +84,27 @@ export class ResultsComponent implements OnInit {
     this.calculateOptimum(this.findOperator())
     .then(results => this.getRenewableShare(results))
     .then(() => this.calculateShare(this.renewableArray, this.totalArray))
-    .then(shares => this.findOptimum(shares))
+    .then(shares => this.sortShares(shares))
+    .then(rankedShares => this.findOptimumToday(rankedShares))
   }
 
+  // Find the corresponding operator to the area
   findOperator(){
+    let index = 0;
     for(let key in this.areaCodes) {
       if(this.areaCodes[key].indexOf(this.selectedState) >= 0){
         this.selectedOperator = key;
         return key;
       } 
-      else {
-        console.log('State not found yet (Error if shown 5 times!)');
+      else if (index < 4) {
+        index++;
+      } else if (index === 4){
+        console.log('Error: State not found.')
       }
     }
   }
   
-  // get the data for all generationtypes and save the results in one object
+  // Get the data for all generationtypes and save the results in one object
   async calculateOptimum(areaCode){
     for (let key in this.generationTypes){
       await this.energyDataService.getData(this.generationTypes[key], areaCode )
@@ -110,31 +134,39 @@ export class ResultsComponent implements OnInit {
       }
     }
 
-    calculateShare(renArray, totArray) {
-      this.shareArray = totArray.map((el, idx) => {
-        // every 4th element, as the resolution of the renewable data is quarterhours --> renArray.length === 96
-        // PumpedHydroShare is added, as average percentile over the last two years; OtherRenewables are added as fixed values (minimal deviation from average); source: "2018-10-25 Generated Power Analysis" --> (c) Bundesnetzagentur | SMARD.de
-        return ((renArray[idx*4])  / el) + this.pumpedHydroShare[idx];
-      })
-      // To-Do: Add the other renewable sources as fixed values (water: 1780, Bio: 4568, Other: 140, Pumped-Hydro: pumpedHydroShare --> hourly)
-      return this.shareArray;
+  calculateShare(renArray, totArray) {
+    let otherRenewables = this.otherRenewables[this.selectedOperator].others;
+    let pumpedHydroArray = this.otherRenewables[this.selectedOperator].pumpedHydro;
+    this.shareArray = totArray.map((el, idx) => {
+      // every 4th element, as the resolution of the renewable data is quarterhours --> renArray.length === 96
+      // PumpedHydroShare is added, as average percentile over the last two years; OtherRenewables are added as fixed values (minimal deviation from average); source: "2018-10-25 Generated Power Analysis" --> (c) Bundesnetzagentur | SMARD.de
+      return ((renArray[idx*4] + otherRenewables + pumpedHydroArray[idx])   / el);
+    })
+    // To-Do: Add the other renewable sources as fixed values (water: 1780, Bio: 4568, Other: 140, Pumped-Hydro: pumpedHydroShare --> hourly)
+    return this.shareArray;
+  }
+
+  // Sorting the array of shares (first element: highest share)
+  sortShares(shaArray) {
+    // populate array from sharesArray to sort it descending
+    this.sortedShareArray = shaArray.slice();
+    this.sortedShareArray.sort((a:number, b:number) => {return b-a});
+    // find the indexes of the sorted values in the original array
+    for(let i = 0; i< this.sortedShareArray.length; i++){
+      this.rankedShares.push(shaArray.indexOf(this.sortedShareArray[i]));
     }
-
-    findOptimum(shaArray) {
-
-      // populate array from sharesArray to sort it descending
-      this.sortedShareArray = shaArray.slice();
-      this.sortedShareArray.sort((a:number, b:number) => {return b-a});
-
-      // find the indexes of the sorted values in the original array
-      for(let i = 0; i< this.sortedShareArray.length; i++){
-        this.sharePositions.push(shaArray.indexOf(this.sortedShareArray[i]));
-      }
-      // saves hours with the highest shares during the day in a descending order
-      return this.sharePositions;
-    }
-          
-          
+    // saves hours with the highest shares during the day in a descending order
+    return this.rankedShares;
+  }
+       
+  // Find the optimum for today, after now
+  findOptimumToday(rankedSharesArray){
+    let now = new Date().getHours();
+    // Finds the first element bigger than the current hour
+    this.optimumToday = rankedSharesArray.find(el => {
+        return el > now; 
+    })
+  }   
           /*
           ONLY NEEDED FOR SMARD-FETCH
           resultsSmard:Object = {};
